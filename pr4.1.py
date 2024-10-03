c0b7b2757030dfd2770cf9044831af5b28434c27
Mikhail-chegrincev <mikhailchegrincev@gmail.com>
2024-10-03 05:51:00 +0300

    PR_4

---
+++ pr4.1.py
@@ -0,0 +1,4 @@
+a, b = int(input()), int(input())
+while a <= b:
+    print(a)
+    a += 1
---
+++ pr4.3.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+while a >= b:
+    if a % 2 == 1:
+        print(a)
+    a -= 1
